f01daff64541ed8bc4f26237e68e3a83ec38eec7
rkpatil37 <77320497+rkpatil37@users.noreply.github.com>
2022-09-16 17:17:00 +0530

    Add files via upload

---
+++ Vss/script.js
@@ -0,0 +1,10 @@
+var cartIcon=document.querySelector(".cart");
+var offerContainer=document.querySelector(".offer_container");
+
+
+
+
+cartIcon.onclick=function () {
+    offerContainer.classList.toggle("new_offer");
+   
+}
